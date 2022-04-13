f6975ac57ddfa4d98783e62a602132f5049184d0
hefabao <hefabao@126.com>
2022-04-13 13:09:03 +0800

    提交: go语言的panic recover

---
+++ amydir/dir_a10/a10_02_test.go
@@ -0,0 +1,33 @@
+package dira10
+
+import (
+	"fmt"
+	"testing"
+)
+
+func withPanic() {
+	fmt.Println("1111")
+	defer func() {
+		fmt.Println("222")
+		recover()
+	}()
+	m := 9
+	n := 0
+	a := m / n
+	_ = a
+	fmt.Println("3333")
+}
+
+func beforePanic() {
+	fmt.Println("before")
+}
+
+func afterPanic() {
+	fmt.Println("after")
+}
+
+func TestYxx(t *testing.T) {
+	beforePanic()
+	withPanic()
+	afterPanic()
+}
